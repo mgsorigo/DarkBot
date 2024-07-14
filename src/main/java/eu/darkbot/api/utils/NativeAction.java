7b6a7fd100bbd023db62fc6c449fcbac6b5b9254
mgucma <marek.gucm@gmail.com>
2024-07-14 21:46:03 +0200

    fix

--- src/main/java/eu/darkbot/api/utils/NativeAction.java
+++ src/main/java/eu/darkbot/api/utils/NativeAction.java
@@ -2,25 +2,23 @@ package eu.darkbot.api.utils;
 
 public class NativeAction {
 
-    private static long toActionLong(short message, short wParam, int lParam, boolean after) {
-        return toActionLong(message, wParam, (short) lParam, (short) (lParam >> 16), after);
+    private static long toActionLong(short message, short wParam, int x, int y, boolean after) {
+        return toActionLong(message, wParam, (short) x, (short) (x >> 16), (short) y, (short) (y >> 16), after);
     }
 
-    //message only 15bits
-    private static long toActionLong(short message, short wParam, short lParamLow, short lParamHigh, boolean after) {
-        long val = lParamLow;
-        val |= (long) lParamHigh << 16;
-        val |= (long) (wParam & 0xffff) << 32;
-        val |= (long) (message & 0xffff) << 48;
-
-        if (after) val |= (long) 1 << 63;
-        else val &= ~((long) 1 << 63);
-
+    private static long toActionLong(short message, short wParam, short lParamLow, short lParamHigh, short yParamLow, short yParamHigh, boolean after) {
+        long val = ((long) lParamHigh << 16) | (lParamLow & 0xFFFF);
+        val |= ((long) yParamHigh << 32) | ((long) yParamLow & 0xFFFF) << 16;
+        val |= ((long) wParam & 0xFFFF) << 48;
+        val |= ((long) message & 0xFFFF) << 56;
+        if (after) {
+            val |= 1L << 63;
+        }
         return val;
     }
 
     public enum Mouse {
-        CLICK(0x1FF), // KekkaPlayer only
+        CLICK(0x1FF),
         MOVE(0x200),
         DOWN(0x201),
         UP(0x202);
@@ -32,17 +30,16 @@ public class NativeAction {
         }
 
         public long of(int x, int y) {
-            return toActionLong(message, (short) 1, (short) x, (short) y, false);
+            return toActionLong(message, (short) 1, x, y, false);
         }
 
-        // can be used after text paste action
         public long after(int x, int y) {
-            return toActionLong(message, (short) 1, (short) x, (short) y, true);
+            return toActionLong(message, (short) 1, x, y, true);
         }
     }
 
     public enum Key {
-        CLICK(0x1FE), // KekkaPlayer only
+        CLICK(0x1FE),
         DOWN(0x100),
         UP(0x101),
         CHAR(0x102);
@@ -54,11 +51,11 @@ public class NativeAction {
         }
 
         public long of(int keyCode) {
-            return toActionLong(message, (short) keyCode, 1, false);
+            return toActionLong(message, (short) keyCode, 1, 0, false); // y=0 for key actions
         }
 
         public long after(int keyCode) {
-            return toActionLong(message, (short) keyCode, 1, true);
+            return toActionLong(message, (short) keyCode, 1, 0, true); // y=0 for key actions
         }
     }
 
@@ -67,11 +64,11 @@ public class NativeAction {
         private static final short MESSAGE = 0x20A;
 
         public static long up(int x, int y) {
-            return toActionLong(MESSAGE, DELTA, (short) x, (short) y, false);
+            return toActionLong(MESSAGE, DELTA, x, y, false);
         }
 
         public static long down(int x, int y) {
-            return toActionLong(MESSAGE, (short) -DELTA, (short) x, (short) y, false);
+            return toActionLong(MESSAGE, (short) -DELTA, x, y, false);
         }
     }
 }
